5f8e402f7d288e3d93704bcca276bf9e5bad9ab0
nancyRana11 <nancyrana2006@gmail.com>
2026-02-13 20:25:58 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,6 +9,9 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -26,7 +29,7 @@ class DataIngestion:
             df=pd.read_csv(r'notebook\data\stud.csv')
             logging.info('Read the dataset as dataframe')
 
-
+    
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
 
@@ -34,6 +37,7 @@ class DataIngestion:
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
 
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
 
             logging.info("ingestion of the data is completed")
 
@@ -51,6 +55,10 @@ if __name__=="__main__":
     train_data,test_data=obj.initiate_data_ingestion()  
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,preprocessor_path=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+
 
     
